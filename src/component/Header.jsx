14b545d02d14a3b36b267bf7437b23ce4a5de3f1
sinarith <k.sinarith5522@gmail.com>
2025-03-25 21:36:51 +0700

    Update

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import DarkModeToggle from './DarkModeToggle';
-import Home from '../pages/Home';
-// import Projects from './pages/Projects';
-// import Contact from './pages/Contact';
-// import About from './pages/About';
 
 const Header = () => {
   return (
     <header className="header">
       <h1>My Portfolio</h1>
-      <Home />
       <DarkModeToggle />
     </header>
   );
