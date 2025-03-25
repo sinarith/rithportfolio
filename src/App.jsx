100811d326c31b088facb9938290712cef5c1402
sinarith <k.sinarith5522@gmail.com>
2025-03-25 21:40:27 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './component/Header';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import About from './pages/About';
+// import Home from './pages/Home';
+// import Projects from './pages/Projects';
+// import Contact from './pages/Contact';
+// import About from './pages/About';
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
-      </Routes>
+      </Routes> */}
     </Router>
   );
 };
