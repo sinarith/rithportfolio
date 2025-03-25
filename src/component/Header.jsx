31dcaf83f32eeaf5fa1aa0ef26ceed8672f3c241
sinarith <k.sinarith5522@gmail.com>
2025-03-25 21:27:12 +0700

    Update

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,42 +1,12 @@
-import { useState } from "react";
-import DarkModeToggle from "./DarkModeToggle";
+import React from 'react';
+import DarkModeToggle from './DarkModeToggle';
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-      <div className="container">
-        <a className="navbar-brand" href="#">My Portfolio</a>
-
-        <button
-          className="navbar-toggler"
-          type="button"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className={`collapse navbar-collapse ${menuOpen ? "show" : ""}`}>
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Projects</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
-
-          <DarkModeToggle />
-        </div>
-      </div>
-    </nav>
+    <header className="header">
+      <h1>My Portfolio</h1>
+      <DarkModeToggle />
+    </header>
   );
 };
 
